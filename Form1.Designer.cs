e8bacdfe9d755bdbb4d4a42f8a271bb3d94729b9
Bahadur Guliyev <bahaguliyev@gmail.com>
2023-08-18 10:16:26 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿
+namespace Rezervasiya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.masa1 = new System.Windows.Forms.Button();
+            this.masa2 = new System.Windows.Forms.Button();
+            this.masa3 = new System.Windows.Forms.Button();
+            this.masa4 = new System.Windows.Forms.Button();
+            this.masa5 = new System.Windows.Forms.Button();
+            this.masa6 = new System.Windows.Forms.Button();
+            this.masa7 = new System.Windows.Forms.Button();
+            this.masa8 = new System.Windows.Forms.Button();
+            this.masa9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // masa1
+            // 
+            this.masa1.BackColor = System.Drawing.Color.Ivory;
+            this.masa1.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masa1.Location = new System.Drawing.Point(12, 24);
+            this.masa1.Name = "masa1";
+            this.masa1.Size = new System.Drawing.Size(100, 90);
+            this.masa1.TabIndex = 0;
+            this.masa1.Text = "Masa 1";
+            this.masa1.UseVisualStyleBackColor = false;
+            this.masa1.Click += new System.EventHandler(this.masa1_Click);
+            // 
+            // masa2
+            // 
+            this.masa2.BackColor = System.Drawing.Color.Ivory;
+            this.masa2.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masa2.Location = new System.Drawing.Point(118, 24);
+            this.masa2.Name = "masa2";
+            this.masa2.Size = new System.Drawing.Size(100, 90);
+            this.masa2.TabIndex = 1;
+            this.masa2.Text = "Masa 2";
+            this.masa2.UseVisualStyleBackColor = false;
+            this.masa2.Click += new System.EventHandler(this.masa2_Click);
+            // 
+            // masa3
+            // 
+            this.masa3.BackColor = System.Drawing.Color.Ivory;
+            this.masa3.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masa3.Location = new System.Drawing.Point(224, 24);
+            this.masa3.Name = "masa3";
+            this.masa3.Size = new System.Drawing.Size(100, 90);
+            this.masa3.TabIndex = 2;
+            this.masa3.Text = "Masa 3";
+            this.masa3.UseVisualStyleBackColor = false;
+            this.masa3.Click += new System.EventHandler(this.masa3_Click);
+            // 
+            // masa4
+            // 
+            this.masa4.BackColor = System.Drawing.Color.Ivory;
+            this.masa4.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masa4.Location = new System.Drawing.Point(12, 120);
+            this.masa4.Name = "masa4";
+            this.masa4.Size = new System.Drawing.Size(100, 90);
+            this.masa4.TabIndex = 5;
+            this.masa4.Text = "Masa 4";
+            this.masa4.UseVisualStyleBackColor = false;
+            this.masa4.Click += new System.EventHandler(this.masa4_Click);
+            // 
+            // masa5
+            // 
+            this.masa5.BackColor = System.Drawing.Color.Ivory;
+            this.masa5.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masa5.Location = new System.Drawing.Point(117, 120);
+            this.masa5.Name = "masa5";
+            this.masa5.Size = new System.Drawing.Size(100, 90);
+            this.masa5.TabIndex = 4;
+            this.masa5.Text = "Masa 5";
+            this.masa5.UseVisualStyleBackColor = false;
+            this.masa5.Click += new System.EventHandler(this.masa5_Click);
+            // 
+            // masa6
+            // 
+            this.masa6.BackColor = System.Drawing.Color.Ivory;
+            this.masa6.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masa6.Location = new System.Drawing.Point(224, 120);
+            this.masa6.Name = "masa6";
+            this.masa6.Size = new System.Drawing.Size(100, 90);
+            this.masa6.TabIndex = 3;
+            this.masa6.Text = "Masa 6";
+            this.masa6.UseVisualStyleBackColor = false;
+            this.masa6.Click += new System.EventHandler(this.masa6_Click);
+            // 
+            // masa7
+            // 
+            this.masa7.BackColor = System.Drawing.Color.Ivory;
+            this.masa7.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masa7.Location = new System.Drawing.Point(12, 216);
+            this.masa7.Name = "masa7";
+            this.masa7.Size = new System.Drawing.Size(100, 90);
+            this.masa7.TabIndex = 8;
+            this.masa7.Text = "Masa 7";
+            this.masa7.UseVisualStyleBackColor = false;
+            this.masa7.Click += new System.EventHandler(this.masa7_Click);
+            // 
+            // masa8
+            // 
+            this.masa8.BackColor = System.Drawing.Color.Ivory;
+            this.masa8.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masa8.Location = new System.Drawing.Point(118, 216);
+            this.masa8.Name = "masa8";
+            this.masa8.Size = new System.Drawing.Size(100, 90);
+            this.masa8.TabIndex = 7;
+            this.masa8.Text = "Masa 8";
+            this.masa8.UseVisualStyleBackColor = false;
+            this.masa8.Click += new System.EventHandler(this.masa8_Click);
+            // 
+            // masa9
+            // 
+            this.masa9.BackColor = System.Drawing.Color.Ivory;
+            this.masa9.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masa9.Location = new System.Drawing.Point(224, 216);
+            this.masa9.Name = "masa9";
+            this.masa9.Size = new System.Drawing.Size(100, 90);
+            this.masa9.TabIndex = 6;
+            this.masa9.Text = "Masa 9";
+            this.masa9.UseVisualStyleBackColor = false;
+            this.masa9.Click += new System.EventHandler(this.masa9_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Ivory;
+            this.textBox1.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(457, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 30);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(413, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 23);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(375, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 23);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nömrəsi";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Ivory;
+            this.listBox1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 27;
+            this.listBox1.Location = new System.Drawing.Point(15, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(406, 382);
+            this.listBox1.TabIndex = 14;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.BackColor = System.Drawing.Color.Ivory;
+            this.maskedTextBox1.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(457, 98);
+            this.maskedTextBox1.Mask = "(+000) 00-000-00-00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(174, 30);
+            this.maskedTextBox1.TabIndex = 13;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.Info;
+            this.button10.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(533, 150);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(98, 31);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "Əlavə et";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(102, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 39);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Rezervasiyanı ləğv et";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Info;
+            this.label3.Location = new System.Drawing.Point(12, 349);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 23);
+            this.label3.TabIndex = 17;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(658, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(436, 455);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1106, 484);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.masa7);
+            this.Controls.Add(this.masa8);
+            this.Controls.Add(this.masa9);
+            this.Controls.Add(this.masa4);
+            this.Controls.Add(this.masa5);
+            this.Controls.Add(this.masa6);
+            this.Controls.Add(this.masa3);
+            this.Controls.Add(this.masa2);
+            this.Controls.Add(this.masa1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RezervasiyaEt";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button masa1;
+        private System.Windows.Forms.Button masa4;
+        private System.Windows.Forms.Button masa5;
+        private System.Windows.Forms.Button masa6;
+        private System.Windows.Forms.Button masa7;
+        private System.Windows.Forms.Button masa8;
+        private System.Windows.Forms.Button masa9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button masa2;
+        private System.Windows.Forms.Button masa3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rezervasiya
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "994";
+        }
+        string masa = "";
+        string masa_reng_ucun = "";
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if(masa_reng_ucun!="" && textBox1.Text!="" && maskedTextBox1.Text!="")
+            { 
+            Button secilmis_masa = this.Controls.Find(masa_reng_ucun, true).FirstOrDefault() as Button;
+            if (secilmis_masa.BackColor != Color.Green)
+            {
+                listBox1.Items.Add(masa + ", " + textBox1.Text + ", " + maskedTextBox1.Text);
+            }
+            secilmis_masa.BackColor = Color.Green;
+            secilmis_masa.ForeColor = Color.White;
+            }
+            else
+            {
+                MessageBox.Show("Məlumatlar tam daxil edilməyib!");
+            }
+            textBox1.Clear();
+            maskedTextBox1.Clear();
+            maskedTextBox1.Text = "994";
+        }
+
+        private void masa1_Click(object sender, EventArgs e)
+        {
+            masa_reng_ucun = masa1.Name;
+            masa = masa1.Text;
+            label3.Text = "Masa 1-i seçdiniz.";
+        }
+
+        private void masa2_Click(object sender, EventArgs e)
+        {
+            masa_reng_ucun = masa2.Name;
+            masa = masa2.Text;
+            label3.Text = "Masa 2-ni seçdiniz.";
+
+        }
+
+        private void masa3_Click(object sender, EventArgs e)
+        {
+            masa_reng_ucun = masa3.Name;
+            masa = masa3.Text;
+            label3.Text = "Masa 3-ü seçdiniz.";
+
+        }
+
+        private void masa4_Click(object sender, EventArgs e)
+        {
+            masa_reng_ucun = masa4.Name;
+            masa = masa4.Text;
+            label3.Text = "Masa 4-ü seçdiniz.";
+
+        }
+
+        private void masa5_Click(object sender, EventArgs e)
+        {
+            masa_reng_ucun = masa5.Name;
+            masa = masa5.Text;
+            label3.Text = "Masa 5-i seçdiniz.";
+        }
+
+        private void masa6_Click(object sender, EventArgs e)
+        {
+            masa_reng_ucun = masa6.Name;
+            masa = masa6.Text;
+            label3.Text = "Masa 6-nı seçdiniz.";
+
+        }
+
+        private void masa7_Click(object sender, EventArgs e)
+        {
+            masa_reng_ucun = masa7.Name;
+            masa = masa7.Text;
+            label3.Text = "Masa 7-ni seçdiniz.";
+
+        }
+
+        private void masa8_Click(object sender, EventArgs e)
+        {
+            masa_reng_ucun = masa8.Name;
+            masa = masa8.Text;
+            label3.Text = "Masa 8-i seçdiniz.";
+
+        }
+
+        private void masa9_Click(object sender, EventArgs e)
+        {
+            masa_reng_ucun = masa9.Name;
+            masa = masa9.Text;
+            label3.Text = "Masa 9-u seçdiniz.";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem.ToString() != "")
+            {
+                string[] masani_acmaq_ucun = listBox1.SelectedItem.ToString().Split(' ');
+                Button acmaq_ucun_btn = this.Controls.Find("masa" + masani_acmaq_ucun[1].Trim(','), true).FirstOrDefault() as Button;
+                acmaq_ucun_btn.BackColor = Color.Ivory;
+                acmaq_ucun_btn.ForeColor = Color.Black;
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Silmək üçün masa seçin");
+            }
+        }
+    }
+}
